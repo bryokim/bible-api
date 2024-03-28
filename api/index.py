77171149d3a3a3b5d7f7289a5db4255528f181f6
Brian Kim <bryo.kim1@gmail.com>
2024-03-28 21:42:48 +0300

    add /api/verse route to index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,26 @@
-from fastapi import FastAPI, Request, status
+from typing import Union
+
+from fastapi import Depends, FastAPI, Request, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
+from pythonbible.errors import InvalidVerseError
+
 from api._bible.router import bible_router
+
+from api._bible.dependencies import (
+    normalize_bible_version,
+    validate_book,
+    validate_chapter,
+    validate_verse,
+)
+from api._bible.schemas import (
+    AcceptedVersion,
+    VerseResponse,
+    AcceptedBookGroup,
+)
+from api._bible.service import get_parsed_verse
 
 app = FastAPI()
 
@@ -27,4 +44,30 @@ def root():
     return "Welcome to holy-text api"
 
 
-app.include_router(bible_router, prefix='/api')
+@app.get("/api/verse")
+async def verse(
+    book: str = Depends(validate_book),
+    chapter: int = Depends(validate_chapter),
+    verse: str = Depends(validate_verse),
+    book_group: Union[AcceptedBookGroup, None] = AcceptedBookGroup.ANY,
+    bible_version: Union[AcceptedVersion, None] = Depends(
+        normalize_bible_version
+    ),
+) -> VerseResponse:
+    full_verse = f"{book.strip()} {chapter}:{verse.strip()}"
+
+    try:
+        (_book, _chapter), verse_text = get_parsed_verse(
+            full_verse, bible_version
+        )
+    except InvalidVerseError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
+    return {
+        "reference": "{} {}:{}".format(_book, _chapter, verse),
+        "verse_text": verse_text,
+        "version": bible_version,
+    }
+
+
+app.include_router(bible_router, prefix="/api")
