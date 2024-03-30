abd99e461170d73a7883350297b1dabc49fb2c3b
Brian Kim <bryo.kim1@gmail.com>
2024-03-30 19:01:01 +0300

    feat: add CORS
    Allow requests from bible-ui frontend only and localhost if set.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
+import os
+
 from fastapi import FastAPI, Request, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 from src.bible.router import bible_router
 
 app = FastAPI()
+
+LOCALHOST = os.getenv("BIBLE_UI_HOST", "")
+
+origins = ["bible-ui-two.vercel.app"]
+
+if LOCALHOST:
+    origins.append(LOCALHOST)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
