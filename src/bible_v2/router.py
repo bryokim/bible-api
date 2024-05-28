a12e59a3cc469b36f5807f77168637c1a12876e8
Brian Kim <bryo.kim1@gmail.com>
2024-05-28 20:17:28 +0300

    feat: add logic for getting a specific verse

--- src/bible_v2/router.py
+++ src/bible_v2/router.py
@@ -1,5 +1,7 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException, Query, status
+
+from pythonbible.errors import InvalidVerseError
 
 from src.dependencies import (
     validate_book,
@@ -9,7 +11,10 @@ from src.dependencies import (
     validate_random_chapter,
     validate_random_book,
 )
-from src.schemas import AcceptedBookGroup, AcceptedVersion
+from src.schemas import AcceptedBookGroup, AcceptedVersion, VerseResponse
+from src.service import (
+    get_parsed_verse,
+)
 
 router = APIRouter(prefix="/bible", tags=["bible v2"])
 
@@ -58,18 +63,28 @@ async def get_from_reference(
     }
 
 
-@router.get("/{book}/{chapter}/{verse}")
-async def verse(
+@router.get(
+    "/{book}/{chapter}/{verse}",
+    response_model=VerseResponse,
+    status_code=status.HTTP_200_OK,
+)
+async def get_verse(
     book: str = Depends(validate_book),
     chapter: int = Depends(validate_chapter),
     verse: str = Depends(validate_verse),
     book_group: AcceptedBookGroup = AcceptedBookGroup.ANY,
     bible_version: AcceptedVersion = AcceptedVersion.NIV,
-):
+) -> VerseResponse:
+    reference = f"{book.strip()} {chapter}:{verse.strip()}"
+
+    try:
+        (_, _), verse_text = get_parsed_verse(reference, bible_version)
+    except InvalidVerseError as e:
+        raise HTTPException(status_code=404, detail=e.message)
+
     return {
-        "book": book,
-        "chapter": chapter,
-        "verse": verse,
-        "book_group": book_group.value,
+        "reference": reference,
+        "verse_text": verse_text,
+        "book_group": book_group.value if book_group else "",
         "bible_version": bible_version,
     }
